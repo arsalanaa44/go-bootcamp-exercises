bc6aff16b474d334d92466d9da596868bc49b0f1
arsalanaa <mehran.mah44@gmail.com>
2023-03-18 10:55:29 +0330

    test-cases in cases_test.go

--- hw2 exercism/leap-year/leap-by-year_test.go
+++ hw2 exercism/leap-year/leap-by-year_test.go
@@ -1,24 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIsLeapYear(t *testing.T) {
-	var testCases = []struct {
-		description string
-		year        int
-		expected    bool
-	}{
-		{
-			description: "year not divisible by 4 in common year",
-			year:        2015,
-			expected:    false,
-		},
-		{
-			description: "year divisible by 2, not divisible by 4 in common year",
-			year:        1970,
-			expected:    true, //for test
-		},
-	}
 
 	for _, v := range testCases {
 		result := IsLeapYear(v.year)
