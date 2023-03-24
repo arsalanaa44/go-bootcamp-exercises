54ee4bde835834341d4454ab09d5f69bdea41874
arsalanaa <mehran.mah44@gmail.com>
2023-03-24 20:10:24 +0330

    playing with colors

--- test_project/main.go
+++ test_project/main.go
@@ -1,15 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"test_project/textcolor"
+)
 
 func main() {
 
-	i := 0
-start:
-	fmt.Println(i)
-	i++
-	if i <= 7 {
-		goto start
+	colorReset := "\033[0m"
+
+	colorGreen := "\033[32m"
+	colorYellow := "\033[33m"
+	colorBlue := "\033[34m"
+	colorPurple := "\033[35m"
+	colorCyan := "\033[36m"
+	colorWhite := "\033[37m"
+
+	fmt.Println(string(textcolor.ColorRed) + "test")
+	fmt.Println("another test")
+	fmt.Println(string(colorGreen), "test")
+	fmt.Println(string(colorYellow), "test")
+	fmt.Println(string(colorBlue), "test")
+	fmt.Println(string(colorPurple), "test")
+	fmt.Println(string(colorWhite), "test")
+	fmt.Println(string(colorCyan), "test", string(colorReset))
+	fmt.Println("next")
+
+	for i := 0; i < 50; i++ {
+
+		fmt.Println(i, string("\033["+strconv.Itoa(i)+"m"), "color")
 	}
-	fmt.Println("finished!")
 }
